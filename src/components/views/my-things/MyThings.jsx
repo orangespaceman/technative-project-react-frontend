d931e7505da5b7abea8b6f28c3be3099df56faf0
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-07-04 12:11:48 +0100

    Render treasure and what bag it is in

--- src/components/views/my-things/MyThings.jsx
+++ src/components/views/my-things/MyThings.jsx
@@ -6,43 +6,50 @@ import { setBreadcrumb } from "../../../slices/breadcrumbSlice";
 import styles from "./my-things.module.css";
 
 const MyThings = () => {
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const things = useSelector((state) => state.things.userThings);
-  const status = useSelector((state) => state.things.userStatus);
-  const error = useSelector((state) => state.things.userError);
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const things = useSelector((state) => state.things.userThings);
+    const status = useSelector((state) => state.things.userStatus);
+    const error = useSelector((state) => state.things.userError);
 
-  useEffect(() => {
-    dispatch(fetchMyThings());
-  }, [dispatch, location]);
+    useEffect(() => {
+        dispatch(fetchMyThings());
+    }, [dispatch, location]);
 
-  useEffect(() => {
-    dispatch(
-      setBreadcrumb([{ label: "Home", url: "/" }, { label: "My things" }])
+    useEffect(() => {
+        dispatch(
+            setBreadcrumb([{ label: "Home", url: "/" }, { label: "My things" }])
+        );
+    }, [dispatch]);
+
+    return (
+        <div className={styles.wrapper}>
+            {status === "loading" && <div>Loading...</div>}
+            {status === "failed" && <div>{error}</div>}
+            <ul className={styles.list}>
+                {things.map((thing) => (
+                    <li className={styles.item} key={thing.id}>
+                        <div className={styles.itemContent}>
+                            <p>Treasure: {thing.treasurename}</p>
+                            <p>Bag: {thing.bagname}</p>
+                            <NavLink
+                                className={styles.link}
+                                to={`${thing.id}/edit/`}
+                            >
+                                Edit
+                            </NavLink>
+                            <NavLink
+                                className={styles.link}
+                                to={`${thing.id}/delete/`}
+                            >
+                                Delete
+                            </NavLink>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-  }, [dispatch]);
-
-  return (
-    <div className={styles.wrapper}>
-      {status === "loading" && <div>Loading...</div>}
-      {status === "failed" && <div>{error}</div>}
-      <ul className={styles.list}>
-        {things.map((thing) => (
-          <li className={styles.item} key={thing.id}>
-            <div className={styles.itemContent}>
-              <p>{thing.name}</p>
-              <NavLink className={styles.link} to={`${thing.id}/edit/`}>
-                Edit
-              </NavLink>
-              <NavLink className={styles.link} to={`${thing.id}/delete/`}>
-                Delete
-              </NavLink>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 };
 
 export default MyThings;
