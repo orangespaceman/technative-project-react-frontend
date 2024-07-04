701429b9d842acf864d2a41ad28bec9d627cdfb1
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-07-04 12:45:33 +0100

    Add delete bad (not working)

--- src/components/views/delete-thing/DeleteThing.jsx
+++ src/components/views/delete-thing/DeleteThing.jsx
@@ -5,14 +5,21 @@ import { fetchThings, deleteThing } from "../../../slices/thingsSlice";
 import { setBreadcrumb } from "../../../slices/breadcrumbSlice";
 import { showToast } from "../../../slices/toastSlice";
 import styles from "./delete-thing.module.css";
+import {  fetchBags, deleteBag } from "../../../slices/bagSlice";
 
 const DeleteThing = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const thing = useSelector((state) =>
     state.things.items.find((thing) => thing.id === parseInt(id))
   );
+
+  const bag = useSelector((state) =>
+    state.bags.items.find((bag) => bag.id === parseInt(id))
+  );
+  console.log(bag)
 
   useEffect(() => {
     dispatch(
@@ -30,9 +37,22 @@ const DeleteThing = () => {
     }
   }, [thing, dispatch]);
 
+  useEffect(() => {
+    if (!bag) {
+      dispatch(fetchBags());
+    }
+  }, [bag, dispatch]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     await dispatch(deleteThing(thing.id));
+    dispatch(showToast(`Thing deleted`));
+    navigate("/my-things/");
+  };
+
+  const handleBagSubmit = async (e) => {
+    e.preventDefault();
+    await dispatch(deleteBag(bag.id));
     dispatch(showToast(`Thing deleted`));
     navigate("/my-things/");
   };
@@ -42,6 +62,10 @@ const DeleteThing = () => {
       <form onSubmit={handleSubmit}>
         <p>{thing && thing.name}</p>
         <button type="submit">Delete thing</button>
+      </form>
+      <form onSubmit={handleBagSubmit}>
+        <p>{thing && thing.name}</p>
+        <button type="submit">Delete bag</button>
       </form>
     </div>
   );
--- src/slices/bagSlice.js
+++ src/slices/bagSlice.js
@@ -3,19 +3,19 @@ import apiService from "../services/apiService";
 
 //Get all bags
 export const fetchBags = createAsyncThunk("bags/fetchBags", async () => {
-  const response = await apiService("bags", { method: "GET" });
-  return response;
+    const response = await apiService("bags", { method: "GET" });
+    return response;
 });
 
 // Fetching all bags belonging to one user:
 export const fetchBagByUser = createAsyncThunk(
-  "bags/fetchBagByUser",
-  async (userId) => {
-    const response = await apiService("bags/fetchByUser", {
-      method: "GET",
-    });
-    return response;
-  }
+    "bags/fetchBagByUser",
+    async (userId) => {
+        const response = await apiService("bags/fetchByUser", {
+            method: "GET",
+        });
+        return response;
+    }
 );
 
 // export const fetchMyBag = createAsyncThunk(
@@ -30,15 +30,15 @@ export const fetchBagByUser = createAsyncThunk(
 
 //Gets one bag
 export const fetchBag = createAsyncThunk("bags/fetchBag", async (id) => {
-  return await apiService(`bags/${id}`, { method: "GET" });
+    return await apiService(`bags/${id}`, { method: "GET" });
 });
 
 export const addBag = createAsyncThunk("bags/addBag", async (newBag) => {
-  const response = await apiService("bags", {
-    method: "POST",
-    body: JSON.stringify(newBag),
-  });
-  return response;
+    const response = await apiService("bags", {
+        method: "POST",
+        body: JSON.stringify(newBag),
+    });
+    return response;
 });
 
 // export const editThing = createAsyncThunk(
@@ -52,89 +52,86 @@ export const addBag = createAsyncThunk("bags/addBag", async (newBag) => {
 //     }
 // );
 
-// export const deleteThing = createAsyncThunk(
-//     "bags/deleteThing",
-//     async (id) => {
-//         await apiService(`my-Bag/${id}`, { method: "DELETE" });
-//         return id;
-//     }
-// );
+export const deleteBag = createAsyncThunk("bags/deleteBag", async (id) => {
+    await apiService(`bag/${id}`, { method: "DELETE" });
+    return id;
+});
 
 const BagSlice = createSlice({
-  name: "bags",
-  initialState: {
-    items: [],
-    userBags: [],
-    currentThing: null,
-    status: "idle",
-    userBagStatus: "idle",
-    currentBagstatus: "idle",
-    error: null,
-    userBagError: null,
-    currentThingError: null,
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchBags.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(fetchBags.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        // state.items = ["hello i am wrong", "and i suck"];
-        state.items = action.payload;
-      })
-      .addCase(fetchBags.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-      .addCase(fetchBagByUser.pending, (state) => {
-        state.userBagStatus = "loading";
-      })
-      .addCase(fetchBagByUser.fulfilled, (state, action) => {
-        state.userBagStatus = "succeeded";
-        state.userBags = action.payload;
-      })
-      .addCase(fetchBagByUser.rejected, (state, action) => {
-        state.userBagStatus = "failed";
-        state.userBagError = action.error.message;
-      })
-      // .addCase(fetchMyBag.pending, (state) => {
-      //   state.userBagStatus = "loading";
-      // })
-      // .addCase(fetchMyBag.fulfilled, (state, action) => {
-      //   state.userBagStatus = "succeeded";
-      //   state.userBag = action.payload;
-      // })
-      // .addCase(fetchMyBag.rejected, (state, action) => {
-      //   state.userBagStatus = "failed";
-      //   state.userBagError = action.error.message;
-      // })
-      .addCase(fetchBag.pending, (state) => {
-        state.currentBagstatus = "loading";
-      })
-      .addCase(fetchBag.fulfilled, (state, action) => {
-        state.currentBagstatus = "succeeded";
-        state.currentThing = action.payload;
-      })
-      .addCase(fetchBag.rejected, (state, action) => {
-        state.currentBagstatus = "failed";
-        state.currentThingError = action.error.message;
-      });
-    // .addCase(addThing.fulfilled, (state, action) => {
-    //     state.items.push(action.payload);
-    // })
-    // .addCase(editThing.fulfilled, (state, action) => {
-    //     const index = state.items.findIndex(
-    //         (item) => item.id === action.payload.id
-    //     );
-    //     state.items[index] = action.payload;
-    // })
-    // .addCase(deleteThing.fulfilled, (state, action) => {
-    //     state.items = state.items.filter(
-    //         (item) => item.id !== action.payload
-    //     );
-    // });
-  },
+    name: "bags",
+    initialState: {
+        items: [],
+        userBags: [],
+        currentThing: null,
+        status: "idle",
+        userBagStatus: "idle",
+        currentBagstatus: "idle",
+        error: null,
+        userBagError: null,
+        currentThingError: null,
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchBags.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(fetchBags.fulfilled, (state, action) => {
+                state.status = "succeeded";
+                // state.items = ["hello i am wrong", "and i suck"];
+                state.items = action.payload;
+            })
+            .addCase(fetchBags.rejected, (state, action) => {
+                state.status = "failed";
+                state.error = action.error.message;
+            })
+            .addCase(fetchBagByUser.pending, (state) => {
+                state.userBagStatus = "loading";
+            })
+            .addCase(fetchBagByUser.fulfilled, (state, action) => {
+                state.userBagStatus = "succeeded";
+                state.userBags = action.payload;
+            })
+            .addCase(fetchBagByUser.rejected, (state, action) => {
+                state.userBagStatus = "failed";
+                state.userBagError = action.error.message;
+            })
+            // .addCase(fetchMyBag.pending, (state) => {
+            //   state.userBagStatus = "loading";
+            // })
+            // .addCase(fetchMyBag.fulfilled, (state, action) => {
+            //   state.userBagStatus = "succeeded";
+            //   state.userBag = action.payload;
+            // })
+            // .addCase(fetchMyBag.rejected, (state, action) => {
+            //   state.userBagStatus = "failed";
+            //   state.userBagError = action.error.message;
+            // })
+            .addCase(fetchBag.pending, (state) => {
+                state.currentBagstatus = "loading";
+            })
+            .addCase(fetchBag.fulfilled, (state, action) => {
+                state.currentBagstatus = "succeeded";
+                state.currentThing = action.payload;
+            })
+            .addCase(fetchBag.rejected, (state, action) => {
+                state.currentBagstatus = "failed";
+                state.currentThingError = action.error.message;
+            });
+        // .addCase(addThing.fulfilled, (state, action) => {
+        //     state.items.push(action.payload);
+        // })
+        // .addCase(editThing.fulfilled, (state, action) => {
+        //     const index = state.items.findIndex(
+        //         (item) => item.id === action.payload.id
+        //     );
+        //     state.items[index] = action.payload;
+        // })
+        // .addCase(deleteThing.fulfilled, (state, action) => {
+        //     state.items = state.items.filter(
+        //         (item) => item.id !== action.payload
+        //     );
+        // });
+    },
 });
 
 export default BagSlice.reducer;
